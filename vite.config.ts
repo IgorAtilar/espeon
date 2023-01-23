cae26c2ae1cdf1af658a9021e237d0b12a442612
Igor Atilar <igoratilar10@gmail.com>
2023-01-22 21:34:48 -0300

    build: change base name on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
-  base: 'espeon',
+  base: '/espeon/',
   root,
   build: {
     rollupOptions: {
