1c9d485e52fbafbaf178e06761357ba38519b26f
Igor Atilar <igoratilar10@gmail.com>
2023-01-22 22:31:58 -0300

    feat: change bottom nav links

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import './style.css';
 const addActiveStyleToCurrentLink = () => {
   const bottomNavigationLinks = document.querySelectorAll('.nav-link');
 
+  const mapHrefToPath: Record<string, string> = {
+    '.': '/espeon/',
+    'deck.html': '/espeon/deck.html',
+    'ajuda.html': '/espeon/ajuda.html',
+  };
+
   bottomNavigationLinks.forEach((link) => {
     const path = window.location.pathname;
-    const linkHref = link.getAttribute('href');
-    if (path === linkHref) {
+    const linkHref = link.getAttribute('href')!;
+
+    if (mapHrefToPath[linkHref] === path) {
       link.classList.add('bottom-nav-anchor-active');
     }
   });
