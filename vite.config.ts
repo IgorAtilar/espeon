5d77c0876ba8c1446e9f0ada44cf95c13ec085b8
Igor Atilar <igoratilar10@gmail.com>
2023-01-22 21:33:09 -0300

    build: change build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
+  base: 'espeon',
   root,
   build: {
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
+        ajuda: resolve(root, 'ajuda', 'index.html'),
+        deck: resolve(root, 'deck', 'index.html'),
       },
     },
+    outDir,
   },
 });
